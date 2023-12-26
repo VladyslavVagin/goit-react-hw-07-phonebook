04e64889c83e2814ef9be434522b7f778e76eacd
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-12-26 18:33:45 +0100

    update
    filter working

--- src/components/ListOfContacts/ListOfContacts.jsx
+++ src/components/ListOfContacts/ListOfContacts.jsx
@@ -1,12 +1,15 @@
 import css from './ListOfContacts.module.css';
 import { useSelector, useDispatch } from 'react-redux';
-import { selectContacts } from '../../redux/selectors';
+import { selectContacts, selectQuery } from '../../redux/selectors';
 import trashIcon from '../../icons/trash.png';
 import { deleteContact } from '../../redux/operations';
 
 const ListOfContacts = () => {
   const dispatch = useDispatch();
   const items = useSelector(selectContacts);
+  const filter = useSelector(selectQuery);
+
+  const filteredContacts = items.filter(item => item.name.toLowerCase().includes(filter));
 
   return (
     <div className={css.tableContainer}>
@@ -19,7 +22,7 @@ const ListOfContacts = () => {
           </tr>
         </thead>
         <tbody className={css.tbody}>
-          {items.map(({ id, name, phone }) => {
+          {filteredContacts.map(({ id, name, phone }) => {
             return (
               <tr key={id} className={css.tr}>
                 <td className={css.td}>{name}</td>
